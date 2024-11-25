5bd623b6e0f77db4e7fbf17c3dadd9576d606bca
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-11-25 10:28:21 +0530

    Update Transfering.c

--- Transfering.c
+++ Transfering.c
@@ -41,51 +41,12 @@ void* thread_func(void* arg) {
 }
 
 // Parallel programming for 1D loop
-void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads) {
-    numThreads --;
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    pthread_t tid[numThreads];
-    thread_args args[numThreads];
-    
-    int range = high-low;
-    int chunk = range/numThreads;
-    int r=range%numThreads;
-    chunk++;
-    
-    //Below for loop gives first r functions chunk+1 size and others chunk size to optimally handle remainders.
-    for (int i=0,j=0; i<numThreads; i++,j++){
-        if (i==r){
-            low+=(i*chunk);
-            chunk--;
-            j=0;
-        }
-        args[i].low1=low+(j*chunk);
-        args[i].high1=((low+(j+1)*chunk) > high ? high: low+(j+1)*chunk);
-        args[i].lambda=&lambda;
-        int rc = pthread_create(&tid[i],NULL,thread_func,(void*)&args[i]);
-        if (rc){
-            fprintf(stderr, "Error: pthread_create() failed for thread %d\n", i);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    //waiting for threads to terminate.
-    for (int i=0; i<numThreads; i++){
-        int rc = pthread_join(tid[i],NULL);
-        if (rc){
-            fprintf(stderr, "Error: pthread_join() failed for thread %d\n", i);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end_time - start_time;
-    printf("Execution time: %f seconds\n", elapsed.count());
+void parallel_for(int low, int high, auto lambda, int numThreads) {
+    parallel_for(low,high,-1,-1, lambda, numThreads);
 }
 
 // Parallel programming for 2D loop
-void parallel_for(int low1, int high1, int low2, int high2, std::function<void(int, int)> &&lambda, int numThreads) {
+void parallel_for(int low1, int high1, int low2, int high2, auto lambda, int numThreads) {
     numThreads --;
     auto start_time = std::chrono::high_resolution_clock::now();
 
